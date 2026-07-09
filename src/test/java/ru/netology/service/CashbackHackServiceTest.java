13366c88fe8f1e30a368c7faf4a477296852ebaf
andrewquinceee <andrewquinceee@gmail.com>
2026-07-09 08:35:35 +0300

    Update CashbackHackServiceTest.java
    Fix JUnit4 tests - replace TestNG with JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,31 +1,31 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
-
+    
     @Test
     public void shouldReturn100WhenAmountIs900() {
         CashbackHackService service = new CashbackHackService();
         int expected = 100;
         int actual = service.remain(900);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
-
+    
     @Test
     public void shouldReturn0WhenAmountIs1000() {
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
         int actual = service.remain(1000);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
-
+    
     @Test
     public void shouldReturn500WhenAmountIs2500() {
         CashbackHackService service = new CashbackHackService();
         int expected = 500;
         int actual = service.remain(2500);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
